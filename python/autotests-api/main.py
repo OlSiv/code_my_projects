64acdb7ad9d2a833da115ec4de5cfed050f98ea6
ol_siv <sojmoscow@gmail.com>
2026-01-21 00:58:18 +0300

    20-01-2026___1

--- python/autotests-api/main.py
+++ python/autotests-api/main.py
@@ -1,1 +1,11 @@
-print("Hello, PyCharm!")
+import json
+
+data = {
+    "name": "Мария",
+    "age": 25,
+    "is_student": True
+}
+
+json_string = json.dumps(data, indent=4)  # Преобразуем Python-объект в JSON-строку
+print(json_string)
+print(type(json_string))
